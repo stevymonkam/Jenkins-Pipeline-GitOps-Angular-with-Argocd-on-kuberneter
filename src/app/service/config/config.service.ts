bf49db85a8fa224725794a014e145232090dd9cd
stevymonkam <stevy.monkam@yahoo.fr>
2025-09-14 04:03:58 +0200

    chang ip adress2

--- src/app/service/config/config.service.ts
+++ src/app/service/config/config.service.ts
@@ -9,7 +9,7 @@ export class ConfigService {
   API_URL: string
   API_URL2: string
   constructor() {
-    this.API_URL='http://109.176.198.187:8086/api';
+    this.API_URL='http://109.176.198.187:30086/api';
     this.API_URL2='http://localhost:8080';
 
     //this.API_URL='https://cors-anywhere.herokuapp.com/http://api.contratti.immobiliz.com/api'; //http://127.0.0.1:8000
